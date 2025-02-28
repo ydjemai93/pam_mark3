774e724d0ad41fa9f5b53f6af05e636fbe2f2e58
ydjemai93 <62611775+ydjemai93@users.noreply.github.com>
2025-02-28 15:40:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+# main.py
+import os
+from twilio_server import create_app
+
+# Crée l'app Flask
+app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
